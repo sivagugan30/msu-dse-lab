c30f90094730e5b3568c9bf753eb0e0ae8fd43d7
sivagugan30 <sivagugan.98@gmail.com>
2025-03-19 13:52:02 -0400

    Create config.py

---
+++ default/config.py
@@ -0,0 +1,16 @@
+
+
+Collections = [
+  "Undergraduate_Chemistry",
+  "Middle and High School_Chemistry"
+]
+Documents = [
+  'CLUE Text May 2017.pdf',
+  'Coding Scheme for Text LDF Responses.docx',
+  'Cooper acid base reasoning JCE.pdf',
+  'LDFs longitudinal.pdf',
+  'LDFs mechanistc reasoning .pdf',
+  'Mechanistic reasoning LDFs Cooper Becker JCE.pdf',
+  'O-CLUE Text Fall 2019.pdf',
+  'franovic-et-al-2023-how-do-instructors-explain.pdf'
+]
