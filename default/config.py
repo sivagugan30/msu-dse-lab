ab390608aabf125f85969ccff725e570d8967b5f
sivagugan30 <sivagugan.98@gmail.com>
2025-03-21 01:45:06 -0400

    Update config.py

--- default/config.py
+++ default/config.py
@@ -1,16 +1,35 @@
+document_list = {
+    "Undergraduate_Chemistry": [
+        'CLUE Text May 2017.pdf',
+        'Coding Scheme for Text LDF Responses.docx',
+        'Cooper acid base reasoning JCE.pdf',
+        'LDFs longitudinal.pdf',
+        'LDFs mechanistc reasoning .pdf',
+        'Mechanistic reasoning LDFs Cooper Becker JCE.pdf',
+        'O-CLUE Text Fall 2019.pdf',
+        'franovic-et-al-2023-how-do-instructors-explain.pdf'
+    ],
+    "Middle and High School_Chemistry": [
+        'AssessModel.pdf',
+        'DeBarger etal Constructing Assessment Item.pdf',
+        'DesignPrinciples_3DAssessment.pdf',
+        'DesingScientificExplanationTasks.pdf',
+        'Evaluating3DTasks.pdf',
+        'Gotwals_LP_Assessment.pdf',
+        'Harris et al_Assessment.pdf',
+        'Harris et al_NSTABook.pdf',
+        'Hickey_3DLearning.pdf',
+        'KnowingWhatStudentKnow.pdf',
+        'NGSS_PhysicalScience.pdf',
+        'Pellogrino_TrasferableKnowledge.pdf',
+        'ScientificModeling_Mayer.pdf',
+        'SystemsforStateScienceAssessment.pdf',
+        'Unpacking.pdf',
+        'UnpackingCCC.pdf',
+        'UnpackingSEP.pdf',
+        'UnpackingSEP_Examples.pdf',
+        'UsableKnowledge_Assessment.pdf'
+    ]
+}
 
-
-Collections = [
-  "Undergraduate_Chemistry",
-  "Middle and High School_Chemistry"
-]
-Documents = [
-  'CLUE Text May 2017.pdf',
-  'Coding Scheme for Text LDF Responses.docx',
-  'Cooper acid base reasoning JCE.pdf',
-  'LDFs longitudinal.pdf',
-  'LDFs mechanistc reasoning .pdf',
-  'Mechanistic reasoning LDFs Cooper Becker JCE.pdf',
-  'O-CLUE Text Fall 2019.pdf',
-  'franovic-et-al-2023-how-do-instructors-explain.pdf'
-]
+collection_list = list(document_list.keys())
